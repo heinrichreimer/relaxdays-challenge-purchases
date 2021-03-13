565ac594c4143b6e4bc309f141101d30f8798b40
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-13 03:09:33 +0100

    Implement filtering

--- purchases/app.py
+++ purchases/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flasgger import Swagger, swag_from
-from typing import List
+from typing import List, Dict
 from purchases.purchase import Purchase
 
 app = Flask(__name__)
@@ -8,27 +8,45 @@ swagger = Swagger(app)
 purchases: List[Purchase] = []
 
 
+def dict_to_purchase(purchase: Dict) -> Purchase:
+    return Purchase(
+        purchase["lieferant"],
+        purchase["articleID"],
+        purchase["menge"],
+    )
+
+
+def purchase_to_dict(purchase: Purchase) -> Dict:
+    return {
+        "lieferant": purchase.supplier,
+        "articleID": purchase.article_id,
+        "menge": purchase.amount,
+    }
+
+
 @app.route("/purchase", methods=["POST"])
 @swag_from("add_purchase_spec.yml", validation=True)
 def add_purchase():
-    purchase = Purchase(
-        request.json["lieferant"],
-        request.json["articleID"],
-        request.json["menge"],
-    )
-    purchases.append(purchase)
+    purchases.append(dict_to_purchase(request.json))
     return ""
 
 
 @app.route("/purchases", methods=["GET"])
 @swag_from("get_purchases_spec.yml")
 def get_purchases():
-    results = [
-        {
-            "lieferant": purchase.supplier,
-            "articleID": purchase.article_id,
-            "menge": purchase.amount,
-        }
-        for purchase in purchases
-    ]
-    return jsonify(results)
+    result = purchases
+    result = list(map(purchase_to_dict, result))
+    return jsonify(result)
+
+
+@app.route("/purchasesForArticle/<articleID>", methods=["GET"])
+@swag_from("get_purchases_for_article_spec.yml")
+def get_purchases_for_article(articleID):
+
+    def is_correct_article_id(purchase: Purchase):
+        return purchase.article_id == int(articleID)
+
+    result = purchases
+    result = list(filter(is_correct_article_id, result))
+    result = list(map(purchase_to_dict, result))
+    return jsonify(result)
